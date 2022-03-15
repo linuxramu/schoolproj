dbd147effe4d791735e5c6e4f8149a6a8df05371
Ramu Gurrapu <gurrapur@ebrd.com>
2022-03-15 21:42:59 +0000

    Basic file writing is working

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # School Project
-This project is track attandance
+This project is to track attandance
+
+# Pre-requisites
+1. Install Python
+
+# How to Run Project
+```
+python main.py
+```
+
 
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 st_name = input("Please Enter Student Name : ")
 att_date = input("Please Enter Date : ")
+is_new_sheet = input("Do you want to create a new attandance sheet ? yes/no ")
+f = open("new_file.csv", 'w')
+f.write(st_name + "," + att_date )
 
