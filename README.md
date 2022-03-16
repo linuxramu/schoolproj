91e6044348de0fccf8f721f5333b90925e059155
Ramu Gurrapu <gurrapur@ebrd.com>
2022-03-16 17:37:30 +0000

    finished first milestone

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This project is to track attandance
 ```
 python main.py
 ```
+
+# Result
+If you choose an option to create new file -> the file will be created in the same folder - sample file : new_file_CURRENT_DATE_TIME.csv
+If you choose to write into an exiting file -> please make sure the file is in same folder and mention the full file name (including file extension)
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+from datetime import date, datetime
+
+curr_date_time = datetime.now().strftime("%d%m%Y%H%M%S")
+
 st_name = input("Please Enter Student Name : ")
 att_date = input("Please Enter Date : ")
 is_new_sheet = input("Do you want to create a new attandance sheet ? yes/no ")
 if is_new_sheet=='yes':
-    f = open("new_file.csv", 'w')
+    file_name = "new_file_" + curr_date_time + ".csv"
+    f = open(file_name, 'w')
     f.write(st_name + "," + att_date )
     print (" *** Success ***")
     f.close()
 else:
     is_existing_sheet = input("Do you want to write in existing in file ? yes/no ")
     if is_existing_sheet=='yes':
-        file_name = input("Please Exact Name of the File : ")
-        f = open(file_name, 'a')
+        existing_file_name_e = input("Please Exact Name of the File : ")
+        f = open(existing_file_name_e, 'a')
         f.write("\n")
         f.write(st_name + "," + att_date )
         f.close()
         print (" *** Success ***")
+    elif is_existing_sheet=='no':
+        print (" *** Wrong Choice : Try again please *** ")
     else:
         print (" *** File not Found ! Thank you for using my application. Bye !!! ***")
